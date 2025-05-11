48a3e0a8721318bc7de466ce5b955168a010a518
Jay Patel <58739048+warriorjp@users.noreply.github.com>
2025-05-11 18:31:23 +0530

    Update BinarySearch.java

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,35 +1,31 @@
-// Java implementation of recursive Binary Search
-class BinarySearch {
+public class BinarySearch {
 
-	int binarySearch(int arr[], int low, int high, int x)
-	{
-		if (high >= low) {
-			int mid = low + (high - low) / 2;
-			
-			if (arr[mid] == x) {
-				return mid;
-			}
-				
-			if (arr[mid] > x) {
-				return binarySearch(arr, low, mid - 1, x);
-			}
+    public static int binarySearch(int[] arr, int target) {
+        int left = 0, right = arr.length - 1;
 
-			return binarySearch(arr, mid + 1, high, x);
-		}
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
 
-		return -1;
-	}
-	public static void main(String args[])
-	{
-		BinarySearch ob = new BinarySearch();
-		int arr[] = { 2, 3, 4, 10, 40 };  //array
-		int n = arr.length;              //length
-		int x = 	2;                               //element
-		int result = ob.binarySearch(arr, 0, n - 1, x);
-		if (result == -1)
-			System.out.println("Element not present");
-		else
-			System.out.println("Element found at index "
-							+ result);
-	}
+            if (arr[mid] == target)
+                return mid;
+            else if (arr[mid] < target)
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+
+        return -1; // target not found
+    }
+
+    public static void main(String[] args) {
+        int[] sortedArray = {2, 4, 7, 10, 14, 18, 21};
+        int target = 10;
+
+        int result = binarySearch(sortedArray, target);
+
+        if (result != -1)
+            System.out.println("Element found at index: " + result);
+        else
+            System.out.println("Element not found.");
+    }
 }
