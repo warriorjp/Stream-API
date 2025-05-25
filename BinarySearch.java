82622322ab1704281d18e4e647396c3285dc314f
Jay Patel <58739048+warriorjp@users.noreply.github.com>
2025-05-25 16:02:30 +0530

    Update BinarySearch.java

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,21 +1,16 @@
-public class BinarySearch {
-
-    public static int binarySearch(int[] arr, int target) {
-        int left = 0, right = arr.length - 1;
-
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-
-            if (arr[mid] == target)
-                return mid;
-            else if (arr[mid] < target)
-                left = mid + 1;
-            else
-                right = mid - 1;
-        }
-
-        return -1; // target not found
+public int binarySearch(int[] arr, int target) {
+    int low = 0, high = arr.length - 1;
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+        if (arr[mid] == target)
+            return mid;
+        else if (arr[mid] < target)
+            low = mid + 1;
+        else
+            high = mid - 1;
     }
+    return -1; // Not found
+}
 
     public static void main(String[] args) {
         int[] sortedArray = {2, 4, 7, 10, 14, 18, 21};
