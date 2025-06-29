7c48b90b4b11ad90bdee9f4aedb8847c9f63d74a
Jay Patel <58739048+warriorjp@users.noreply.github.com>
2025-06-29 13:46:00 +0530

    Update SIngeltonClass.java

--- SIngeltonClass.java
+++ SIngeltonClass.java
@@ -27,3 +27,19 @@ class Singleton {
 
 	}
 }
+
+To breack the singleton design pattern we can use serializable and clonable interface 
+And to prevent it from breaking we use
+Serializable - readResolver() method 
+Clonable  - override clone methods and throw CloneNotSupportedException
+
+// Protect against serialization
+    protected Object readResolve() {
+        return getInstance();
+    }
+
+    // Protect against cloning
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        throw new CloneNotSupportedException("Cloning not supported for singleton");
+    }
