f022464ef442c892a61eff4293a4a40f18fe2a5e
Jay Patel <58739048+warriorjp@users.noreply.github.com>
2026-07-08 10:33:08 +0530

    Refactor Student class for natural ordering
    Refactored Student class to implement Comparable interface and removed separate comparator classes.

--- Java-Prep/ComparableExample.java
+++ Java-Prep/ComparableExample.java
@@ -1,82 +1,54 @@
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
+import java.util.*;
+import java.lang.*;
 
-class Student {
+class Student implements Comparable<Student> {
+	private int id;
+	private String name;
+	private int marks;
 
-    private int id;
-    private String name;
-    private int marks;
+	public Student(int id, String name, int marks) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.marks = marks;
+	}
 
-    public Student(int id, String name, int marks) {
-        this.id = id;
-        this.name = name;
-        this.marks = marks;
-    }
+	public int getId() {
+		return id;
+	}
 
-    public int getId() {
-        return id;
-    }
+	public void setId(int id) {
+		this.id = id;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public int getMarks() {
-        return marks;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    @Override
-    public String toString() {
-        return id + " " + name + " " + marks;
-    }
-}
+	public int getMarks() {
+		return marks;
+	}
 
-// Sort by Name (Ascending)
-class SortByName implements Comparator<Student> {
+	public void setMarks(int marks) {
+		this.marks = marks;
+	}
 
-    @Override
-    public int compare(Student s1, Student s2) {
-        return s1.getName().compareTo(s2.getName());
+	@Override
+	public int compareTo(Student s) {
+		if (marks == s.marks) {
+			return 0;
+		} else if (marks > s.marks) {
+			return 1;
+		} else {
+			return -1;
+		}
 
-        // Descending
-        // return s2.getName().compareTo(s1.getName());
-    }
-}
-
-// Sort by Marks (Descending)
-class SortByMark implements Comparator<Student> {
-
-    @Override
-    public int compare(Student s1, Student s2) {
-        return Integer.compare(s2.getMarks(), s1.getMarks());
-
-        // Ascending
-        // return Integer.compare(s1.getMarks(), s2.getMarks());
-    }
-}
-
-public class ComparatorExample {
-
-    public static void main(String[] args) {
-
-        ArrayList<Student> students = new ArrayList<>();
-
-        students.add(new Student(11, "Abhijit", 88));
-        students.add(new Student(12, "Suraj", 78));
-        students.add(new Student(13, "Pranav", 68));
-        students.add(new Student(15, "Bhushan", 98));
-
-        // Sort by Name
-        Collections.sort(students, new SortByName());
-
-        System.out.println("Sorted by Name:");
-        students.forEach(System.out::println);
-
-        // Sort by Marks
-        Collections.sort(students, new SortByMark());
-
-        System.out.println("\nSorted by Marks:");
-        students.forEach(System.out::println);
-    }
+		// OR
+		//return Integer.compare(this.age, s.age);
+	}
 }
